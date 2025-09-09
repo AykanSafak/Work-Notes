9a99da8b5839f0f3c12a2d334c781d91b9147ffb
BuildTools <unconfigured@null.spigotmc.org>
2025-09-09 17:59:12 +0300

    Add project files.

---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    internal class ProductDal
+    {
+        public List<Product> GetAll() 
+        {
+            //@ tamamen string olarak kaydet
+            //initial catalog = hangi katalog
+
+            //Sql server'a bağlanmamız lazım ancak zaten bağlı olup olmadığını kontrol ettirmemiz lazımdır.
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog= ETrade;integrated security=true");
+            if (connection.State==ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products",connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            
+            DataTable dataTable=new DataTable();
+        }
+
+    }
+}
